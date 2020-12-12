4a6a6c3f88bdbd00790877b5412dc14409dcd691
kienz7777 <kienvu7777@gmail.com>
2020-12-12 09:46:21 +0700

    add jsconfig to direct link-file

--- src/features/Photo/index.jsx
+++ src/features/Photo/index.jsx
@@ -1,8 +1,9 @@
+import NotFound from 'components/NotFound';
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import NotFound from '../../components/NotFound';
 import AddEditPage from './pages/AddEdit';
 import MainPage from './pages/Main';
+
 
 Photo.propTypes = {};
 
